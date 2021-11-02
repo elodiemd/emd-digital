218d7ddf57d9e676a951b798a056cef8bc1b2b06
Elodie Duffau <elodie.duffau@gmail.com>
2021-11-02 14:19:13 +0100

    small changes of content and design bug

--- contactform.php
+++ contactform.php
@@ -5,11 +5,11 @@ if (isset($_POST['submit'])) {
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo = "contact@emdigitalstudio.com";
+  $mailTo = "emd.digitalstudio@gmail.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an email from".$name.".\n\n".$message;
 
   mail($mailTo, $subject, $text, $headers);
-  header("Location: index.html?mailsend");
+  header("Location: index.html?mailsent");
 }
 ?>
