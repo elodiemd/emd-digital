7ee7e60fdedbf300eae0e0855d6d4816ab981c4f
Elodie Duffau <elodie.duffau@gmail.com>
2021-11-02 16:27:09 +0100

    finished

--- contactform.php
+++ contactform.php
@@ -1,15 +1,16 @@
 <?php
+
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $subject = $_POST['subject'];
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo = "emd.digitalstudio@gmail.com";
+  $mailTo = "contact@emdigitalstudio.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an email from".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $text, $headers);
+  mail($mailTo,$subject,$txt,$headers);
   header("Location: index.html?mailsent");
 }
 ?>
